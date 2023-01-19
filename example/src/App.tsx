93a17e7180d944b4a0ed39c971477b382614fe1d
yujin jeong <sinijeong.only@gmail.com>
2023-01-19 11:09:27 +0900

    Revert "example: init routing"
    This reverts commit 486cdd0f70190c27e13f2bf6ca4437ec3b93559a.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,15 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import List from './list';
+import Dashboard from '@mylinks/dashboard';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/list/:id' element={<List />} />
-        <Route index element={<>Home</>} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Dashboard />;
 }
 
 export default App;
